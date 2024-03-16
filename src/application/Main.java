fcf180125dc8a15bb74a623430a178f845cfe79d
Khandakar227 <xenonknight54@gmail.com>
2024-03-17 03:19:46 +0600

    Dark mode

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,13 +12,18 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-//			Font.loadFont(getClass().getResourceAsStream("resources/UthmanicScriptHAFS.otf"), 24);		
-			Parent root = FXMLLoader.load(getClass().getResource("/Home.fxml"));
+			//Font.loadFont(getClass().getResourceAsStream("resources/UthmanicScriptHAFS.otf"), 24);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/Home.fxml"));
+			Parent root = loader.load();
 			Scene scene = new Scene(root,500,700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
-			primaryStage.getIcons().add(new Image("/application/resources/App-icon.png"));
+			RootPaneController rootPaneController = loader.getController();
+			rootPaneController.setScene(scene);
 			
+			// Set application icon
+			primaryStage.getIcons().add(new Image("/application/resources/App-icon.png"));
+		
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- src/application/RootPaneController.java
+++ src/application/RootPaneController.java
@@ -1,32 +1,42 @@
 package application;
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ResourceBundle;
 
 import components.SurahCard;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 
-public class RootPaneController {
+public class RootPaneController implements Initializable {
+	private Scene scene;
 	@FXML
 	VBox root_pane;
 	@FXML
 	TextField search_field;
+	@FXML
+	Button dark_mode_btn;
 	
 	private FlowPane flowPane;
-	private int iterCount = 0;
-
-	 public void initialize() {
+	private boolean isDarkMode = false;
+	
+	public void initialize(URL arg0, ResourceBundle arg1) {
 		if(root_pane == null) {
 			System.out.println("No root_pane");
 			return;
@@ -41,8 +51,32 @@ public class RootPaneController {
 		root_pane.getChildren().add(scrollPane);
 		
 		getAllSurah(flowPane);
+		
+		// Dark mode button function
+		dark_mode_btn.setOnAction(e -> {
+			if(!isDarkMode) {
+				scene.getStylesheets().add("/application/resources/dark-theme.css");
+				setButtonIcon(e, "/application/resources/night-mode.png");
+				isDarkMode = true;
+			} else {
+				scene.getStylesheets().remove("/application/resources/dark-theme.css");
+				setButtonIcon(e, "/application/resources/day-mode.png");
+				isDarkMode = false;
+			}
+		});
 	 }
 	 
+
+	void setButtonIcon(ActionEvent e, String url) {
+		Image icon = new Image(getClass().getResource(url).toExternalForm());
+		ImageView imageView = new ImageView(icon);
+		imageView.setFitHeight(30);
+		imageView.setFitWidth(30);
+		Button btn = (Button) e.getTarget();
+		btn.setGraphic(imageView);
+	}
+	
+	
 	 private void getAllSurah(FlowPane flowPane) {
 		Connection connection = DbController.getInstance();	
 		try {
@@ -73,5 +107,9 @@ public class RootPaneController {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	 }
+	 
+	 public void setScene(Scene scene) {
+        this.scene = scene;
 	 }
 }
